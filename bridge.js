6f1182cf8096ff5591e0e71f6095d44c19658c3d
kianalee <55262780+kianalee@users.noreply.github.com>
2026-04-08 16:20:23 -0400

    Refactor bridge.js to add new database tables and endpoints

--- bridge.js
+++ bridge.js
@@ -14,31 +14,54 @@ db.exec(`
     CREATE TABLE IF NOT EXISTS pressure_readings (
         id INTEGER PRIMARY KEY,
         value REAL NOT NULL,
-        timestamp DATETIME DEFAULT (datetime('now', 'localtime'))
+        timestamp DATETIME DEFAULT (strftime('%Y-%m-%d %H:%M:%f', 'now', 'localtime'))  
     );
     CREATE TABLE IF NOT EXISTS flow_readings (
         id INTEGER PRIMARY KEY,
         value REAL NOT NULL,
-        timestamp DATETIME DEFAULT (datetime('now', 'localtime'))
+        timestamp DATETIME DEFAULT (strftime('%Y-%m-%d %H:%M:%f', 'now', 'localtime'))  
     );
     CREATE TABLE IF NOT EXISTS system_updates (
         id INTEGER PRIMARY KEY,
         topic TEXT NOT NULL,
         value TEXT NOT NULL,
-        timestamp DATETIME DEFAULT (datetime('now', 'localtime'))
+        timestamp DATETIME DEFAULT (strftime('%Y-%m-%d %H:%M:%f', 'now', 'localtime'))  
+    );
+    CREATE TABLE IF NOT EXISTS session_starts (
+        id INTEGER PRIMARY KEY,
+        timestamp DATETIME DEFAULT (strftime('%Y-%m-%d %H:%M:%f', 'now', 'localtime'))  
+    );
+    CREATE TABLE IF NOT EXISTS leak_threshold(
+        id INTEGER PRIMARY KEY,
+        value text NOT NULL,
+        timestamp DATETIME DEFAULT (strftime('%Y-%m-%d %H:%M:%f', 'now', 'localtime'))  
+    );
+    CREATE TABLE IF NOT EXISTS block_threshold(
+        id INTEGER PRIMARY KEY,
+        value text NOT NULL,
+        timestamp DATETIME DEFAULT (strftime('%Y-%m-%d %H:%M:%f', 'now', 'localtime'))  
+    );
+    CREATE TABLE IF NOT EXISTS latency (
+        id INTEGER PRIMARY KEY,
+        value TEXT NOT NULL
     );
 `);
 
 console.log('SQLite database ready');
 
 // ─── MQTT Bridge ───────────────────────────────────────────────
-const mqttClient = mqtt.connect('mqtt://192.168.4.2:1883', {
+const mqttClient = mqtt.connect('mqtt://10.229.170.200:1883', {
     username: 'master',
     password: 'masterpass'
 });
 
 mqttClient.on('connect', () => {
     console.log('Connected to MQTT broker');
+
+    db.prepare(`
+        INSERT INTO session_starts DEFAULT VALUES
+    `).run();
+
     mqttClient.subscribe('esp32/pressure', { nl: true });
     mqttClient.subscribe('esp32/flow', {nl:true});
     mqttClient.subscribe('esp32/pump', {nl:true});
@@ -46,11 +69,15 @@ mqttClient.on('connect', () => {
     mqttClient.subscribe('esp32/bottom_valve', {nl:true});
     mqttClient.subscribe('system_ready', {nl:true});
     mqttClient.subscribe('system_status', {nl:true});
-    mqttClient.subscribe('override_status', {nl:true});
+    mqttClient.subscribe('system_pause', {nl:true});
+    mqttClient.subscribe('block_threshold', {nl:true});
+    mqttClient.subscribe('leak_threshold', {nl:true});
+    mqttClient.subscribe('esp32/latency_test', {nl:true});
+
 });
 
 mqttClient.on('message', (topic, message) => {
-    if (topic =='esp32/pressure' || topic =='esp32/flow'){
+    if (topic =='esp32/pressure' || topic =='esp32/flow'|| topic =='block_threshold' || topic =='leak_threshold') {
         const value = parseFloat(message.toString());
         if (isNaN(value)) {
             console.warn(`Invalid value received on ${topic}: ${message.toString()}`);
@@ -66,8 +93,37 @@ mqttClient.on('message', (topic, message) => {
             insert.run(value);
             console.log(`Saved [flow]: ${value} L/min`);
         }
-
+        else if(topic =='block_threshold'){
+            const insert = db.prepare('INSERT INTO block_threshold (value) VALUES (?)');
+            insert.run(value);
+            console.log(`Saved [block_threshold]: ${value}`);
+        }
+        else if(topic =='leak_threshold'){
+            const insert = db.prepare('INSERT INTO leak_threshold (value) VALUES (?)');
+            insert.run(value);
+            console.log(`Saved [leak_threshold]: ${value}`);
+        }
     }
+    else if(topic == "esp32/latency_test"){
+            const t0 = parseInt (message.toString());
+            if (isNaN(t0)) {
+                console.warn(`Invalid latency value: ${message.toString()}`);
+                return;
+            }   
+            const t1 = Date.now(); // when bridge recieves the message
+            const data = {t0: t0, t1: t1};
+
+            const insert = db.prepare('INSERT INTO latency (value) VALUES (?)');
+            insert.run(JSON.stringify(data));
+
+            const t2 = Date.now(); 
+            data.t2 = t2;
+
+
+            const update = db.prepare('UPDATE latency SET value = ? WHERE id = (SELECT MAX(id) FROM latency)');
+            update.run(JSON.stringify(data));
+            //console.log(`Saved [latency]: ${data}`);
+        }
     else{
         const insert = db.prepare('INSERT INTO system_updates (topic, value) VALUES (?, ?)');
         insert.run(topic, message.toString());
@@ -134,6 +190,13 @@ app.get('/api/system_updates', (req, res) => {
     res.json(rows);
 });
 
+// Get latest reading for a SPECIFIC system topic (e.g., /api/system/system_status)
+app.get('/api/system/latest/:topic', (req, res) => {
+    const topic = req.params.topic;
+    const rows = db.prepare('SELECT * FROM system_updates WHERE topic = ? ORDER BY timestamp DESC LIMIT 1').get(topic);
+    res.json(rows);
+});
+
 // Get updates for a SPECIFIC system topic (e.g., /api/system/system_status)
 app.get('/api/system/:topic', (req, res) => {
     const topic = req.params.topic;
@@ -152,10 +215,88 @@ app.get('/api/system/latest/all', (req, res) => {
     res.json(rows);
 });
 
+app.get('/api/water_usage/latest', (req, res) => {
+
+    const session = db.prepare(`
+        SELECT timestamp
+        FROM session_starts
+        ORDER BY timestamp DESC
+        LIMIT 1
+    `).get();
+
+    if (!session) {
+        return res.json({
+            litres: 0,
+            message: "no session found"
+        });
+    }
+
+    const rows = db.prepare(`
+        SELECT value, timestamp
+        FROM flow_readings
+        WHERE timestamp >= ?
+        ORDER BY timestamp ASC
+    `).all(session.timestamp);
+
+    if (rows.length < 2) {
+        return res.json({
+            litres: 0,
+            since: session.timestamp
+        });
+    }
+
+    let totalLitres = 0;
+
+    for (let i = 1; i < rows.length; i++) {
+
+        const prev = rows[i - 1];
+        const curr = rows[i];
+
+        const t0 = new Date(prev.timestamp).getTime();
+        const t1 = new Date(curr.timestamp).getTime();
+
+        const deltaMinutes =
+            (t1 - t0) / 60000;
+
+        // trapezoidal integration
+        const avgFlow =
+            (prev.value + curr.value) / 2;
+
+        totalLitres +=
+            avgFlow * deltaMinutes;
+    }
+
+    res.json({
+        value: totalLitres,
+        timestamp: session.timestamp
+    });
+
+});
+
 // Delete all readings (useful for testing)
 app.delete('/api/system_updates', (req, res) => {
     db.prepare('DELETE FROM system_updates').run();
     res.json({ message: 'All readings deleted' });
+});
+
+//─── REST API Endpoints (threshold values) ────────────────────────────────────────
+app.get('/api/thresholds/leak/latest', (req, res) => {
+    const row = db.prepare('SELECT * FROM leak_threshold ORDER BY timestamp DESC LIMIT 1').get();
+    res.json(row); 
+});
+//─── REST API Endpoints (threshold values) ────────────────────────────────────────
+app.get('/api/thresholds/block/latest', (req, res) => {
+    const row = db.prepare('SELECT * FROM block_threshold ORDER BY timestamp DESC LIMIT 1').get();
+    res.json(row); 
+});
+
+app.get('/api/latency/latest', (req, res) => {
+    const row = db.prepare(`
+        SELECT * FROM latency ORDER BY id DESC LIMIT 1`).get();
+
+    const data = JSON.parse(row.value);
+
+    res.json(data);
 });
 // ─── REST API Endpoints (Commands) ────────────────────────────────────────
 
